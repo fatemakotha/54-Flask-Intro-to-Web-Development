e06bdc5731d4d426dc9a7e1c5e3826510db23d02
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-16 21:21:57 +0600

    Nested functions

--- 449 Python Functions as First Class Objects-Passing & Nesting Functions.py
+++ 449 Python Functions as First Class Objects-Passing & Nesting Functions.py
@@ -12,4 +12,17 @@ def divide(n1, n2):
 
 #First class objects, can be passed around as arguements e.g. int/string/float etc.
 def calculate(calc_function, n1, n2):
-    calc_function(n1, n2)
+    return calc_function(n1, n2)
+result = calculate(multiply, 2, 3)
+print(result)
+
+#Nested functions:
+def outer_function():
+    print("I am outer")
+    def nested_function():
+        print("I am inner")
+    nested_function()
+# nested_function() #NameError: name 'nested_function' is not defined. Did you mean: 'outer_function'?
+outer_function()
+#PRINTS:# I am outer
+        # I am inner
