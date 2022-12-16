b3d092702b9504472c90abafa129917cbd7532af
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-16 21:11:21 +0600

    Python decorator

--- newfile.py
+++ newfile.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/') #just one / means the user wants to see the home page
+@app.route('/') #just one / means the user wants to see the home page. This is called a PYTHON DECORATOR
 def hello_world():
     return 'Hello, World!'
 
