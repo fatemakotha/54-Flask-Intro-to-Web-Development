125ec7efdf51aa07399466307f3f09a626dcc761
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-16 21:16:44 +0600

    First class objects, can be passed around as arguements e.g. int/string/float etc

--- 449 Python Functions as First Class Objects-Passing & Nesting Functions.py
+++ 449 Python Functions as First Class Objects-Passing & Nesting Functions.py
@@ -9,3 +9,7 @@ def multiply(n1, n2):
 
 def divide(n1, n2):
     return n1 / n2
+
+#First class objects, can be passed around as arguements e.g. int/string/float etc.
+def calculate(calc_function, n1, n2):
+    calc_function(n1, n2)
